52bb2c5361975c8ec89ad3096ce542f2e077c46c
Paul210132 <paul.goalec@gmail.com>
2021-05-07 14:52:50 +0200

    Github Pages deployment: Edited config to generate target content

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,11 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
-import postcss from 'rollup-plugin-postcss';
+import copy from 'rollup-plugin-copy';
 //import indexcss from './src/css/index.css'
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
 const production = !process.env.ROLLUP_WATCH;
-
 export default {
 	input: 'src/js/index.js',
 	output: {
@@ -15,12 +14,19 @@ export default {
 		sourcemap: true
 	},
 	plugins: [
-    // postcss({
-    //   extract: true,
-    //   extract: indexcss.resolve('index.css')
-    // }),
+		// postcss({
+		//   extract: true,
+		//   extract: indexcss.resolve('index.css')
+		// }),
 		resolve(), // tells Rollup how to find date-fns in node_modules
 		commonjs(), // converts date-fns to ES modules
-		production && terser() // minify, but only in production
+		production && terser(), // minify, but only in production
+		copy({
+			targets: [
+        { src: 'src/index.html', dest: ['./public','./'] },
+        { src: 'src/css/index.css', dest: ['./public','./'] },
+        { src: ['./public/bundle.js','./public/bundle.js.map'], dest: './' }
+			]
+		})
 	]
 };
